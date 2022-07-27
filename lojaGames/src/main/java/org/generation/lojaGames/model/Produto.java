f783252d847d00c8693017cfe0101eb6881d8e14
Thayna Saraiva <thaynasaraiva99@gmail.com>
2022-07-27 18:17:51 -0300

    Security

---
+++ lojaGames/src/main/java/org/generation/lojaGames/model/Produto.java
@@ -0,0 +1,87 @@
+package org.generation.lojaGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	// Criação Primary Key - Tabela Produto
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String nome;
+
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String produtora;
+
+	@NotNull
+	private double preco;
+
+	@NotNull
+	private int quant;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProdutora() {
+		return produtora;
+	}
+
+	public void setProdutora(String produtora) {
+		this.produtora = produtora;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuant() {
+		return quant;
+	}
+
+	public void setQuant(int quant) {
+		this.quant = quant;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
